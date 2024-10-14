e3ce5c1fa3b7c1625adaa51f27e789d6fa196795
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-14 19:56:47 +0300

    feature: default .glb files and .png face texture for Character. Add threejs, react-three-fiber, react-three-drei and expo-asset

---
+++ custom.d.ts
@@ -0,0 +1,24 @@
+declare module '*.glb' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.gif' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.exr' { 
+    const value: string; 
+    export default value; 
+}
+
+declare module '*.hdr' { 
+    const value: string; 
+    export default value; 
+}
