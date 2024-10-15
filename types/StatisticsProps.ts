e63bd345b83d3ed5e892e192f3d7737b4792d06c
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-15 23:48:13 +0300

    feature: Statistics props types

---
+++ types/StatisticsProps.ts
@@ -0,0 +1,12 @@
+interface StatisticsProps {
+    id: string;
+    user_id: string;
+    highest_streak: number;
+    habits_done: number;
+    habits_not_done: number;
+    habits_focused_on: number;
+    created_at: string;
+    updated_at: string;
+}
+
+export default StatisticsProps;
