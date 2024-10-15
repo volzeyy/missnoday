3b67ae291a7ceee03abd1491424e75c1a241b6b8
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-15 16:24:09 +0300

    feature: (login) routes completed

--- app/(login)/_layout.tsx
+++ app/(login)/_layout.tsx
@@ -1,9 +1,36 @@
-import { Stack } from 'expo-router'
+import useTheme from '@/hooks/useTheme'
+import { Ionicons } from '@expo/vector-icons'
+import { router, Stack } from 'expo-router'
+import { TouchableOpacity } from 'react-native'
 
 const Layout = () => {
+  const { text, background } = useTheme()
+
   return (
-    <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerShadowVisible: false,
+        contentStyle: {
+          backgroundColor: background, 
+          padding: 10, 
+        },
+        headerStyle: { 
+          backgroundColor: background 
+        },
+        headerLeft: () => (
+          <TouchableOpacity style={{ paddingRight: 10}} onPress={() => {router.dismiss()}}>
+            <Ionicons size={28} color={text} name="arrow-back" />
+          </TouchableOpacity>
+        ),
+      }}
+    >
+        <Stack.Screen 
+          name="index" 
+          options={{ 
+            title: "Log In"
+          }} 
+        />
     </Stack>
   )
 }
--- app/(login)/index.tsx
+++ app/(login)/index.tsx
@@ -1,13 +1,105 @@
-import { StyleSheet, Text, View } from 'react-native'
+import InputField from '@/components/InputField'
+import { router } from 'expo-router'
+import { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import Button from '@/components/Button'
+import { supabase } from '@/config/supabase'
+import useSessionStore from '@/stores/useSessionStore'
 
-const LogIn = () => {
+const Page = () => {
+  const { setSession } = useSessionStore();
+
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const insets = useSafeAreaInsets()
+
+  async function signInWithEmail() {
+    try {
+      setLoading(true)
+  
+      const { data: { session }, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      })
+  
+      if (error && error.message == "Email not confirmed") {
+        const { error } = await supabase.auth.resend({
+          type: 'signup',
+          email: email,
+        })
+  
+        if (error) {
+          throw error.message;
+        }
+  
+        router.navigate("/(register)/verify")
+        return
+      }
+  
+      if (error) {
+        throw error.message
+      }
+  
+      if (session) {
+        setSession(session);
+        handleNavigateToMain();
+      }
+    } catch (error) {
+      alert(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleNavigateToMain = () => {
+    router.replace("/(main)/")
+  }
+  
   return (
-    <View>
-      <Text>LogIn</Text>
+    <View style={[styles.container, {paddingBottom: insets.bottom }]}>
+      <View style={styles.inputContainer}>
+        <InputField 
+          placeholder="Email" 
+          value={email} 
+          type='email'
+          onChangeText={setEmail} 
+        />
+        <InputField 
+          placeholder="Password" 
+          value={password} 
+          type='password'
+          onChangeText={setPassword} 
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button 
+          title="Log In" 
+          onPress={signInWithEmail}
+          isDisabled={!email || !password || loading}
+        />
+      </View>
     </View>
   )
 }
 
-export default LogIn
+export default Page
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 20,
+    alignItems: 'center',
+  },
+  inputContainer: {
+    gap: 20,
+    width: "100%",
+  },
+  buttonContainer: {
+    gap: 20,
+    width: "100%",
+    alignItems: 'center',
+  }
+})
