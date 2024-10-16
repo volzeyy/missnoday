70c6fcb004590354a6041972878a537e6f94d3c5
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-16 21:21:53 +0300

    feature: (main)/contents.tsx page

--- app/(main)/contents.tsx
+++ app/(main)/contents.tsx
@@ -1,13 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import CosmeticChance from '@/components/CosmeticChance';
+import useTheme from '@/hooks/useTheme';
+import useFetchAllCosmetics from '@/hooks/useFetchAllCosmetics';
 
 const Contents = () => {
+  const allCosmetics = useFetchAllCosmetics();
+
+  const chance: number | null = allCosmetics && parseFloat((100 / Object.values(allCosmetics).length).toFixed(2));
+
+  const { background } = useTheme()
+    
   return (
-    <View>
-      <Text>Contents</Text>
+    <View
+        style={[styles.container, {backgroundColor: background}]}
+    >
+        <ScrollView
+            contentContainerStyle={styles.contentContainer}
+        >
+            {allCosmetics && allCosmetics.map(cosmetic => (
+                <CosmeticChance
+                    key={cosmetic.id}
+                    name={cosmetic.name}
+                    type={cosmetic.type}
+                    chance={chance}
+                />
+            ))}
+        </ScrollView>
     </View>
   )
 }
 
-export default Contents
+export default Contents;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: "white"
+    },
+    contentContainer: {
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    }
+})
