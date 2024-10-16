f4102d969a60fd5cdd626be862ec2e34d117ebd7
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-16 18:36:44 +0300

    feature: Tip

---
+++ components/Tip/Tip.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from 'react-native'
+import useTheme from '../../hooks/useTheme';
+
+const Tip = (props: any) => {
+  const { title, tip } = props;
+
+  const { text } = useTheme();
+
+  return (
+    <View
+        style={styles.container}
+    >
+        {title ?
+            <Text style={[styles.title, {color: text}]}>{title}</Text>
+        : null}
+        {tip ?
+            <Text style={[styles.tip, {color: text}]}>{tip}</Text>
+        : null}
+    </View>
+  )
+}
+
+export default Tip
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 5,
+      },
+    title: {
+        fontSize: 24,
+        fontWeight: '800',
+        textAlign: "center",
+    },
+    tip: {
+        fontSize: 16,
+        textAlign: "center",
+    }
+})
