32daf010bcd55dfffd5826cb56075348e76e768a
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-17 23:50:00 +0300

    feature: colors props

---
+++ types/ColorsProps.ts
@@ -0,0 +1,15 @@
+interface ColorsProps {
+    id: string;
+    user_id: string;
+    hat: string | null;
+    hair: string | null;
+    shirt: string | null;
+    pants: string | null;
+    shoes: string | null;
+    background: string | null;
+    skin: string | null;
+    created_at: string;
+    updated_at: string;
+}
+
+export default ColorsProps;
