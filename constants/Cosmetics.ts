143d75f8242d7421bace8d78aa74a009934f6d36
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-17 23:54:27 +0300

    feature: add type prop to COSMETICS

--- constants/Cosmetics.ts
+++ constants/Cosmetics.ts
@@ -5,44 +5,47 @@ import defaultPants from "@/assets/models/defaultPants.glb";
 import defaultShoes from "@/assets/models/defaultShoes.glb";
 import defaultFace from "@/assets/textures/face_default.png";
 
-const COSMETICS: { [key: string]: { name: string, icon?: string, default?: string} } = {
+const COSMETICS: { [key: string]: { name: string, icon?: string, default?: string, type: string} } = {
     all: {
         name: 'All',
         icon: "shimmer",
+        type: "all",
     },
     shirt: {
         name: 'Shirt',
         icon: "tshirt-crew",
-        default: defaultShirt, 
+        type: "shirt",
+        default: defaultShirt,
     },
     pants: {
         name: 'Pants',
         icon: "hanger",
+        type: "pants",
         default: defaultPants,
     },
     hair: {
         name: 'Hair',
         icon: "hair-dryer",
+        type: "hair",
         default: defaultHair,
     },
     face: {
         name: "Face",
         icon: "emoticon",
+        type: "face",
         default: defaultFace,
     },
     hat: {
         name: 'Hat',
         icon: "hat-fedora",
+        type: "hat",
         default: defaultHat,
     },
     shoes: {
         name: 'Shoes',
         icon: "shoe-formal",
+        type: "shoes",
         default: defaultShoes,
-    },
-    background: {
-        icon: "image",
-        name: 'Background',
     },
 };
 
