fd1a64d744ffb74e6763b6a4a48566adbab5edc9
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-22 21:02:02 +0300

    style: changed colors and design

--- components/Statistics/Statistics.tsx
+++ components/Statistics/Statistics.tsx
@@ -1,11 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
-import useFetchStatistics from '@/hooks/useFetchStatistics';
-import useTheme from '@/hooks/useTheme';
+import { StyleSheet, Text, View } from "react-native";
+import useFetchStatistics from "@/hooks/useFetchStatistics";
 
-const Statistics = (props: {user_id: string | undefined}) => {
+const Statistics = (props: { user_id?: string }) => {
   const { user_id } = props;
-
-  const { secondary } = useTheme();
 
   const statisticsData = useFetchStatistics(user_id);
 
@@ -13,51 +10,91 @@ const Statistics = (props: {user_id: string | undefined}) => {
     <View style={styles.container}>
       <View style={styles.rowContainer}>
         <View style={[styles.columnContainer]}>
-            <Text>Highest streak</Text>
-            <Text>{statisticsData?.highest_streak}</Text>
+          <View style={styles.headerContainer}>
+            <Text style={styles.header}>Highest streak</Text>
+          </View>
+          <View style={styles.bodyContainer}>
+            <Text style={styles.body}>{statisticsData?.highest_streak}</Text>
+          </View>
         </View>
         <View style={[styles.columnContainer]}>
-            <Text>Active Habits</Text>
-            <Text>{statisticsData?.habits_focused_on}</Text>
+          <View style={styles.headerContainer}>
+            <Text style={styles.header}>Active Habits</Text>
+          </View>
+          <View style={styles.bodyContainer}>
+            <Text style={styles.body}>{statisticsData?.habits_focused_on}</Text>
+          </View>
         </View>
       </View>
       <View style={styles.rowContainer}>
         <View style={[styles.columnContainer]}>
-            <Text>Habits done</Text>
-            <Text>{statisticsData?.habits_done}</Text>
+          <View style={styles.headerContainer}>
+            <Text style={styles.header}>Habits done</Text>
+          </View>
+          <View style={styles.bodyContainer}>
+            <Text style={styles.body}>{statisticsData?.habits_done}</Text>
+          </View>
         </View>
         <View style={[styles.columnContainer]}>
-            <Text>Habits not done</Text>
-            <Text>{statisticsData?.habits_not_done}</Text>
+          <View style={styles.headerContainer}>
+            <Text style={styles.header}>Habits not done</Text>
+          </View>
+          <View style={styles.bodyContainer}>
+            <Text style={styles.body}>{statisticsData?.habits_not_done}</Text>
+          </View>
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Statistics
+export default Statistics;
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: "auto",
-        gap: 10,
-    },
-    rowContainer: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        gap: 10,
-    },
-    columnContainer: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-start",
-        padding: 10,
-        paddingVertical: 20,
-        borderRadius: 20,
-        flexGrow: 1,
-        backgroundColor: "rgb(225, 224, 227)"
-    }
-})
+  container: {
+    width: "100%",
+    height: "auto",
+    gap: 10,
+  },
+  rowContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 10,
+  },
+  columnContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    borderRadius: 20,
+    padding: 10,
+    paddingVertical: 15,
+    gap: 5,
+    flexGrow: 1,
+    backgroundColor: "black"
+  },
+  headerContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  header: {
+    fontSize: 12,
+    color: "white",
+    fontWeight: "600",
+  },
+  bodyContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  body: {
+    fontSize: 16,
+    color: "white",
+    fontWeight: "800",
+  },
+});
