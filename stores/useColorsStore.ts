d0defcb0bcf4ad53cb080677f4c94a760bf09c6f
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-28 23:48:23 +0200

    feature: modified for local testing

--- stores/useColorsStore.ts
+++ stores/useColorsStore.ts
@@ -2,16 +2,30 @@ import { create } from 'zustand';
 import ColorsProps from '@/types/ColorsProps';
 
 interface ColorsState {
-    colors: ColorsProps | null;
+    colors: Partial<ColorsProps>;
     setColors: (colors: ColorsProps) => void;
     clearColors: () => void;
 }
 
 const useColorsStore = create<ColorsState>((set) => ({
-    colors: null,
+    colors: {
+        hat: null,
+        hair: null,
+        shirt: null,
+        pants: null,
+        shoes: null,
+        background: null,
+    },
     setColors: (colors: ColorsProps) => set({ colors }),
     clearColors: () => set({
-        colors: null,
+        colors: {
+            hat: null,
+            hair: null,
+            shirt: null,
+            pants: null,
+            shoes: null,
+            background: null,
+        },
     }),
 }));
 
