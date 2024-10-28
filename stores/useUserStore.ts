69da1950c6535b731cdfc41651e2bebebf16e695
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-28 23:46:51 +0200

    feature: default values initialized for local testing

--- stores/useUserStore.ts
+++ stores/useUserStore.ts
@@ -2,16 +2,30 @@ import { create } from 'zustand';
 import UserProps from '@/types/UserProps';
 
 interface UserState {
-    user: UserProps | null;
-    setUser: (user: UserProps) => void;
+    user: Partial<UserProps>;
+    setUser: (user: Partial<UserProps>) => void;
     clearUser: () => void;
 }
 
 const useUserStore = create<UserState>((set) => ({
-    user: null,
-    setUser: (user: UserProps) => set({ user }),
+    user: {
+        full_name: "Guest",
+        username: "guest",
+        coins: 1500,
+        streak: 0,
+        streak_freeze: 2,
+        avatar_url: "https://api.dicebear.com/9.x/pixel-art-neutral/png?mouth=happy09&eyes=variant09",
+    },
+    setUser: (user: Partial<UserProps>) => set({ user }),
     clearUser: () => set({
-        user: null,
+        user: {
+            full_name: "Guest",
+            username: "guest",
+            coins: 1500,
+            streak: 0,
+            streak_freeze: 2,
+            avatar_url: "https://api.dicebear.com/9.x/pixel-art-neutral/png?mouth=happy09&eyes=variant09",
+        },
     }),
 }));
 
