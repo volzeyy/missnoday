6e58d5ac741fe9878904d722bac5d642d9c52738
Voldemar Ezerin <voldemarezerin@gmail.com>
2024-10-28 23:48:36 +0200

    feature: modified for local testing

--- stores/useCharacterStore.ts
+++ stores/useCharacterStore.ts
@@ -2,16 +2,32 @@ import { create } from 'zustand';
 import CharacterProps from '@/types/CharacterProps';
 
 interface CharacterState {
-    character: CharacterProps | null;
-    setCharacter: (character: CharacterProps) => void;
+    character: Partial<CharacterProps>;
+    setCharacter: (character: Partial<CharacterProps>) => void;
     clearCharacter: () => void;
 }
 
 const useCharacterStore = create<CharacterState>((set) => ({
-    character: null,
-    setCharacter: (character: CharacterProps) => set({ character }),
+    character: {
+        face: null,
+        hair: null,
+        hat: null,
+        shirt: null,
+        pants: null,
+        shoes: null,
+    },
+    setCharacter: (character: Partial<CharacterProps>) => set({ character }),
     clearCharacter: () => set({
-        character: null,
+        character: {
+            id: undefined,
+            user_id: undefined,
+            face: null,
+            hair: null,
+            hat: null,
+            shirt: null,
+            pants: null,
+            shoes: null,
+        },
     }),
 }));
 
